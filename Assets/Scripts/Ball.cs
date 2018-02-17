ef14e5af9a6ea23c6d117c5f63021b1f92bb8976
danijel <danijel.husakovic@gmail.com>
2018-02-17 10:42:04 +0100

    At start ball follows paddle. Launches on click. The ball bounces. Added just a few bricks. Introduced prefabs.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	
+	private bool isResting = true;
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+	paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isResting){
+		// Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// Wait for mouse click to launch the ball
+			if(Input.GetMouseButtonDown(0)){
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+				isResting = false;
+			}
+		}
+		
+	}
+	
+}
